ba3a87370b67d31319d474d847d6bc5db1975888
lady-neo <thaviny.to.use.games@gmail.com>
2025-12-14 15:57:50 -0300

    Path errado no import

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,19 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 import { defineConfig, passthroughImageService } from "astro/config";
+import path from "node:path";
 
 export default defineConfig({
-  image: {
-    service: passthroughImageService()
-  },
   site: "https://neon-dubs.vercel.app/",
+  image: {
+    service: passthroughImageService(),
+  },
   integrations: [mdx(), sitemap(), icon()],
+  vite: {
+    resolve: {
+      alias: {
+        "@pvt-assets": path.resolve("./src/assets"),
+      },
+    },
+  },
 });
