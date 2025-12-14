027323c2e51ade02dfd2db22372f095a765c5e10
lady-neo <thaviny.to.use.games@gmail.com>
2025-12-14 14:51:50 -0300

    Re-adicionei ./src/content/dubs/, corrigindo erro de build

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,4 @@
 export const SITE_TITLE = "Neon Dubs";
-export const SITE_DESCRIPTION =
-  "Neon Dubs é uma equipe de Fandublagem e Dublagem Original que busca ser conhecida por todo o Brasil pelo seu trabalho com carinho e amor, Criado pelo Fandublador, Artista e Designer GK (Guilherme Mattza) em 2020.";
+export const SITE_DESCRIPTION = "Neon Dubs é uma equipe de Fandublagem e Dublagem Original que busca ser conhecida por todo o Brasil pelo seu trabalho com carinho e amor, Criado pelo Fandublador, Artista e Designer GK (Guilherme Mattza) em 2020.";
 export const SITE_URL = new URL(Astro.url.pathname, Astro.site);
 export const INSTAGRAM = "https://www.instagram.com/neon.dubsoficial";
