a90dde1b2276eea45b296fcd86513941ef8a393f
lady-neo <thaviny.to.use.games@gmail.com>
2025-12-14 14:59:49 -0300

    Código formatado

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,5 @@
 export const SITE_TITLE = "Neon Dubs";
-export const SITE_DESCRIPTION = "Neon Dubs é uma equipe de Fandublagem e Dublagem Original que busca ser conhecida por todo o Brasil pelo seu trabalho com carinho e amor, Criado pelo Fandublador, Artista e Designer GK (Guilherme Mattza) em 2020.";
-export const SITE_URL = new URL(import.meta.env.SITE); 
+export const SITE_DESCRIPTION =
+  "Neon Dubs é uma equipe de Fandublagem e Dublagem Original que busca ser conhecida por todo o Brasil pelo seu trabalho com carinho e amor, Criado pelo Fandublador, Artista e Designer GK (Guilherme Mattza) em 2020.";
+export const SITE_URL = new URL(import.meta.env.SITE);
 export const INSTAGRAM = "https://www.instagram.com/neon.dubsoficial";
